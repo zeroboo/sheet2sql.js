05aacaea3e82e929d73dd4ada4fc248e8aa28a48
Pham Tan Hung <tanhung.pham@nhnent.com>
2018-09-18 14:34:55 +0700

    add test

--- test/test.js
+++ test/test.js
@@ -1,3 +1,49 @@
-var myLogModule = require('xlsx-mysql');
+const excel2Mysql = require("xlsx2mysql");
+excel2Mysql.info("tada");
 
-myLogModule.info('Node.js started');
+var excelFile ="./mission.xlsx";
+var sqlFile ="./insert.sql";
+var sprintf = require('sprintf-js').sprintf;
+var parseArgs = require('minimist');
+
+console.log("----- Parameters:")
+var argv = parseArgs(process.argv.slice(2));
+console.log("excelFile: " + excelFile);
+console.log("sqlFile: " + sqlFile);
+console.log(JSON.stringify(argv));
+
+console.log("----- Reading excel:")
+var config = {
+  "sheets":{
+    "Mission":{"table_name":"poker_mission"
+      , "fields": {
+          "id":"number"
+          ,"name":"text"
+          ,"description":"text"
+          ,"type":"number"
+          ,"category":"number"
+          ,"frequency":"number"
+          , "requirement_type":"number"
+          ,	"parameters":"text"
+          , "active":"number"
+        }
+      , "truncate_table":true}
+    , "MissionLevel":{
+        "table_name":"poker_mission_level"
+        , "fields":{
+            "mission_id":"number"
+            ,"level":"number"
+            ,"params":"text"
+            , "requirement_value":"number"
+            ,	"reward_gold":"number"
+      }
+    , "truncate_table":true}
+  },
+  "post_insert_sql":["SELECT * FROM poker_mission_level;"
+      , "SELECT * FROM poker_mission;"
+      , "SELECT * FROM poker_mission LEFT JOIN poker_mission_level ON poker_mission.id = poker_mission_level.mission_id;"
+    ]
+
+};
+
+excel2Mysql.exportExcelToQuery(excelFile, config, sqlFile);
